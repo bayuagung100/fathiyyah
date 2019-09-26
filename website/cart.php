1373b31340cbb17b21a7a862c17901747b00e521
bayuagung100 <bayuagung100@gmail.com>
2019-09-26 15:15:42 +0700

    link judul cart

--- website/cart.php
+++ website/cart.php
@@ -42,7 +42,7 @@
 							$total += $it;
 				echo'
 					<tr class="item-row">
-						<td><h5>'.$np.'</h5><br><img class="img-product-home text-center lazy" style="max-width:100px;max-height:100px" data-original="'.$pic.'" src="../img/loader.gif"></td>
+						<td><a href="'.$set["url"].'product/'.$ip.'/detail/"><h5>'.$np.'</h5></a><br><img class="img-product-home text-center lazy" style="max-width:100px;max-height:100px" data-original="'.$pic.'" src="../img/loader.gif"></td>
 						<td class="text-right" title="Quantity">'.$jp.'</td>
 						<td class="text-right" title="Price">'.rupiah($hp).'</td>
 						<td class="text-right" title="Items Total">'.rupiah($it).'</td>
