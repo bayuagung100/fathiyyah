01bb2883ec185804975d1a11f2f0480466819c73
bayuagung100 <bayuagung100@gmail.com>
2019-10-02 15:54:24 +0700

    lupa

--- shop/login.php
+++ shop/login.php
@@ -50,7 +50,7 @@ if(empty($_SESSION['user']) or empty($_SESSION['pass']) or $_SESSION['log']==0){
                 }
                 ?>
 				
-				<form method="get" action="<?php echo $set["url"];?>shop/cart.php?act=login">
+				<form method="post" action="">
 					<div class="row">
 						<div class="col-md-2">
 						<label for="fullname">Username</label>
