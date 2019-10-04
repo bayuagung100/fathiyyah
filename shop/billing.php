5e4c330d620ea3214273de7b527e6b5466226fd1
bayuagung100 <bayuagung100@gmail.com>
2019-10-04 15:50:13 +0700

    mail bank

--- shop/billing.php
+++ shop/billing.php
@@ -76,30 +76,35 @@ if ($aksi=='billing') {
     $email_body = "Halo, $mail_name!\n\n";
     $email_body .= "Email ini adalah pemberitahuan Tagihan Anda yang dibuat pada $dtpem[tanggal]\n\n";
     $email_body .= "No.Tagihan: $tagihan\n\n";
-    $email_body .= "Items Tagihan: \n\n";
-    // $query3 = mysqli_query($mysqli,"SELECT * FROM pembelian WHERE no_tagihan='$tagihan' ");
+    
     while ($mail = mysqli_fetch_array($query2)) {
         $mip = $mail['id_produk'];
         $mj = $mail['jumlah'];
         $mu = $mail['ukuran'];
         $mquery = mysqli_query($mysqli,"SELECT * FROM product WHERE id='$mip' ");
         while ($dquery = mysqli_fetch_array($mquery)) {
-            $dqnp = $dquery['nama_product'];
+            // $dqnp = $dquery['nama_product'];
             $dqh = $dquery['harga'];
 
             $it = $mj*$dqh;
             $total += $it;
-            $email_body .= "- $dqnp\nUkuran: $mu\nHarga: ".rupiah($dqh)." x ".$mj." = ".rupiah($it)."\n\n";
-           
+            
         }
     }
     $email_body .= "Total Tagihan: ".rupiah($total)."\n\n";
     $email_body .= "----------------------------------------------------------------------\n\n";
     $email_body .= "Anda dapat melakukan pembayaran ke rekening kami, sebagai berikut:\n";
     $email_body .= "*Mohon sertakan No.Tagihan / Invoice Number, untuk konfirmasi pembayaran.\n\n";
-    $email_body .= "- MANDIRI\n";
-    $email_body .= "No Rekening: \n";
-    $email_body .= "a/n: \n";
+    $bankquery = mysqli_query($mysqli,"SELECT * FROM bank_transfer");
+    while ($b = mysqli_fetch_array($bankquery)) {
+        $nama_bank = $b['nama_bank'];
+        $no_rek = $b['no_rek'];
+        $nama_pemilik = $b['nama_pemilik'];
+
+        $email_body .= "- $nama_bank\n";
+        $email_body .= "No Rekening: $no_rek\n";
+        $email_body .= "a/n: $nama_pemilik\n";
+    }
     $email_body .= "\n\n\n\n";
     $email_body .= "Terima Kasih.";
     $headers = "From: thefathiyyah@erolperkasamandiri.co.id\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
